3b02524a4ad5238d886e7dc0237350a68a3e2af2
Daniil <post@windhelm.ru>
2023-04-22 21:13:00 +0300

    Распределены файлы

--- src/main_test.cpp
+++ src/main_test.cpp
@@ -1,13 +1,9 @@
-#include <gtest/gtest.h>
-
 #include <iostream>
 #include <list>
 
 #include "s21_matrix_oop.h"
 
-int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-
+int main() {
   S21Matrix matrix;
 
   std::cout << matrix.GetCols() << " " << matrix.GetRows() << std::endl;
@@ -17,6 +13,4 @@ int main(int argc, char **argv) {
   std::cout << matrix_2.GetCols() << " " << matrix_2.GetRows() << std::endl;
 
   S21Matrix matrix_3(-3, 5);
-
-  return RUN_ALL_TESTS();
 }
