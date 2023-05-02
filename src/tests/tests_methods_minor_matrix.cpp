0d7c1b0aae0135932af7631d81fdf25cb15f5a44
Daniil <post@windhelm.ru>
2023-05-02 18:37:11 +0300

    Портирован остаток тестов для GetMonirMatrix

--- src/tests/tests_methods_minor_matrix.cpp
+++ src/tests/tests_methods_minor_matrix.cpp
@@ -27,6 +27,24 @@ TEST(tests_methods_get_minor_matrix, correct_arg) {
 }
 
 TEST(tests_methods_get_minor_matrix, correct_arg_2) {
+  S21Matrix matrix(4, 4);
+  S21Matrix answer(3, 3);
+  int i = 0;
+  for (int index_row = 0; index_row < matrix.GetRows(); ++index_row) {
+    for (int index_col = 0; index_col < matrix.GetCols(); ++index_col) {
+      matrix.Assign(index_row, index_col, ++i);
+    }
+  }
+
+  answer.Assign({1, 2, 4, 5, 6, 8, 13, 14, 16});
+
+  S21Matrix minor = matrix.GetMinorMatrix(2, 2);
+
+  EXPECT_TRUE(answer.EqMatrix(minor));
+  EXPECT_TRUE(minor.EqMatrix(answer));
+}
+
+TEST(tests_methods_get_minor_matrix, correct_arg_3) {
   S21Matrix matrix(5, 5);
   S21Matrix answer(4, 4);
   int i = 0;
@@ -36,18 +54,29 @@ TEST(tests_methods_get_minor_matrix, correct_arg_2) {
     }
   }
 
-  i = 6;
-  for (int index_row = 0; index_row < answer.GetRows(); ++index_row) {
-    for (int index_col = 0; index_col < answer.GetCols(); ++index_col) {
-      answer.Assign(index_row, index_col, ++i);
-      if (index_col == answer.GetCols() - 1) {
-        ++i;
-      }
-    }
-  }
+  answer.Assign({1, 2, 3, 4, 6, 7, 8, 9, 11, 12, 13, 14, 16, 17, 18, 19});
+
+  S21Matrix minor = matrix.GetMinorMatrix(4, 4);
+
+  EXPECT_TRUE(answer.EqMatrix(minor));
+  EXPECT_TRUE(minor.EqMatrix(answer));
+}
+
+TEST(tests_methods_get_minor_matrix, correct_arg_4) {
+  S21Matrix matrix(3, 3);
+  S21Matrix answer(2, 2);
+
+  matrix.Assign({2, 5, 7, 6, 3, 4, 5, -2, -3});
+  answer.Assign({3, 4, -2, -3});
 
   S21Matrix minor = matrix.GetMinorMatrix(0, 0);
 
   EXPECT_TRUE(answer.EqMatrix(minor));
   EXPECT_TRUE(minor.EqMatrix(answer));
+}
+
+TEST(tests_methods_get_minor_matrix, uncorrect_arg) {
+  S21Matrix matrix(3, 3);
+  EXPECT_ANY_THROW(matrix.GetMinorMatrix(-1, 2));
+  EXPECT_ANY_THROW(matrix.GetMinorMatrix(1, 4));
 }
